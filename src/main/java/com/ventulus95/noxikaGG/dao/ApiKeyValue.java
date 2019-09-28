ebbe63ad5afb2cb033a5109c22de8e428ee83c8e
ventulus95 <ventulus95@gmail.com>
2019-09-28 18:44:24 +0900

    ReadMe gif update
    리드미에다가 사용자 친화적인 GIF와 스크린 샷 사진들을 넣었습니다.

--- src/main/java/com/ventulus95/noxikaGG/dao/ApiKeyValue.java
+++ src/main/java/com/ventulus95/noxikaGG/dao/ApiKeyValue.java
@@ -1,5 +1,6 @@
 package com.ventulus95.noxikaGG.dao;
 
+//주의 키값 commit하지 말것.
 public class ApiKeyValue {
-	public final static String API_KEY = "RGAPI-2be434c6-c7b3-4e57-a36a-8a2026ee7583";
+	public final static String API_KEY = "RGAPI-c517c19c-a3a1-4a49-9d48-2547ac377744";
 }
